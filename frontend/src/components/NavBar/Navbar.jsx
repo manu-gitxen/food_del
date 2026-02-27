af942ad770bce86c0e9402976e15295b3e05f82f
manu <tonbitx@proton.me>
2026-02-27 17:27:56 +0530

    css border aactive class issue

---
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import { assets } from '../../assets/frontend_assets/assets'
+
+const Navbar = () => {
+    const [menu,setMenu] = useState("home");
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt="logo"  className='logo'/>
+        <ul className='navbar-menu'>
+            <li className={menu === "home"?"active":""}>Home</li>
+            <li className={menu === "Menu"?"active":""}>Menu</li>
+            <li className={menu === "Mobile App"?"active":""}>Mobile App</li>
+            <li className={menu === "Contact Us"?"active":""}>Contact Us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="search icon " />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="" />
+                <div className="dot"></div>
+            </div>
+            <button>Sign In</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
