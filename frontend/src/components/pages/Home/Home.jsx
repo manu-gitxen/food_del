a4aefc24206b0ecb173ec975bca067d40ebdbf7f
manu <tonbitx@proton.me>
2026-03-02 22:58:30 +0530

    completed food card designing next step add product button, vid 1,24,50 m

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./Home.css"
 import Header from '../../Header/Header'
 import ExploreMenu from '../../ExploreMenu/ExploreMenu'
+import { useState } from 'react'
+import FoodDisplay from '../../FoodDisplay/FoodDisplay'
 
 const Home = () => {
+  const [category, setCategory] = useState("All")
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
