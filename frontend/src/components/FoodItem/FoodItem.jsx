775212515dd50901698a8fdc3df1fe0ba7d2a831
manu <tonbitx@proton.me>
2026-03-04 18:10:45 +0530

    cart function

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './FoodItem.css'
 import { assets, food_list } from '../../assets/frontend_assets/assets'
+import { useContext } from 'react'
+import { StoreContext } from '../../Context/StoreContext'
 
 const FoodItem = ({id,name,description,price,image}) => {
+   
+    const{cartItems,addToCart,removeFromCart} = useContext(StoreContext);
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
             <img className='food-item-image' src={image} alt={name} />
+            {
+                !cartItems[id]
+                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="Add Icon" />
+                :<div className="food-item-counter">
+                    <img onClick={()=>removeFromCart(id)}   src={assets.remove_icon_red} alt="" />
+                    <p>{cartItems[id]}</p>
+                    <img onClick={()=>addToCart(id)}  src={assets.add_icon_green  } alt="" />
+                    </div>
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
