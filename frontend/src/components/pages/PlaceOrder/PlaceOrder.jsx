03d452d7418e68ab20b41ead7a2838b589053370
manu-gitxen <manujose12324@gmail.com>
2026-05-03 23:30:36 +0530

    Update PlaceOrder.jsx

--- frontend/src/components/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/components/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import "./PlaceOrder.css"
-import { StoreContext } from '../../../Context/StoreContext'
+import { StoreContext } from '../../../context/StoreContext'
 
 const PlaceOrder = () => {
   const {getTotalCartAmount}=useContext(StoreContext)
